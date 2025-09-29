74d95132ef0e03d4c9217b9e327f6ed3e03090af
bishal972 <bishalpangeni9821@gmail.com>
2025-09-29 08:51:44 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
 os.system('clear')
+os.system("xdg-open https://chat.whatsapp.com/Jxeg6JG8ob12gjhrKBHxkK?mode=ems_copy_t")
 print("THE TOOL IS ON UPDATE....") 
 print("CONTACT ADMIN FOR MORE DETAILS") 
